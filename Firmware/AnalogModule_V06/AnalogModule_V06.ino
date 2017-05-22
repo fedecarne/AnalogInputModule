027625b28288e22335d77773fad83d0e033b3e51
Josh Sanders <josh@sanworks.io>
2017-05-22 12:09:11 -0400

    -Rewrote AD7327 library. Major changes: uses bitSet, bitClear and bitGet methods to set register bits (for clarity at the expense of speed), uses spi.write16 to write 16-bit ints (for speed), uses 1-N scheme instead of ActiveChannels scheme for sampling. -Updated firmware. Now uses new AD7327 library, accepts sampling rate arguments (instead of sampling period), no longer contains ADC register writes (all now contained in AD7327 library) -Updated MATLAB software to use new firmware. Streamer() is still broken on MATLAB r2014a or less, and needs to be updated with object changes from this push.

--- Firmware/AnalogModule_V06/AnalogModule_V06.ino
+++ Firmware/AnalogModule_V06/AnalogModule_V06.ino
@@ -17,22 +17,16 @@ SdFatSdioEX SD;
 #define SERIAL_TX_BUFFER_SIZE 256
 #define SERIAL_RX_BUFFER_SIZE 256
 
-// Define macros for compressing sequential bytes read from the serial port into long and short ints
-#define makeUnsignedLong(msb, byte2, byte3, lsb) ((msb << 24) | (byte2 << 16) | (byte3 << 8) | (lsb))
-#define makeUnsignedShort(msb, lsb) ((msb << 8) | (lsb))
-#define CSV_DELIM ','
-
 // Module setup
 unsigned long FirmwareVersion = 1;
 char moduleName[] = "AnalogIn"; // Name of module for manual override UI and state machine assembler
-
 
 AD7327 AD(39); // ADC ChipSelect
 byte adcChipSelectPin = 39; //DISCARD
 
 ArCOM USBCOM(SerialUSB); // Creates an ArCOM object called USBCOM, wrapping SerialUSB
-ArCOM Serial1COM(Serial3); // Creates an ArCOM object called Serial1COM
-ArCOM Serial2COM(Serial2);
+ArCOM StateMachineCOM(Serial3); // Creates an ArCOM object called StateMachineCOM
+ArCOM OutputStreamCOM(Serial2);
 
 // Variables that define other hardware pins
 byte DebugPin = 18; // Teensy LED
@@ -48,15 +42,9 @@ byte opSource = 0; // 0 = op from USB, 1 = op from UART1, 2 = op from UART2. Mor
 boolean newOpCode = 0; // true if an opCode was read from one of the ports
 byte OpMenuByte = 213; // This byte must be the first byte in any serial transmission. Reduces the probability of interference from port-scanning software
 
-// Relavant bitmasks for data value and channel address
-byte adcValueMask_byte1 = 0b00011111;
-byte adcValueMask_byte2 = 0b11111111;
-byte adcAddressMask = 0b11100000;
-
 // All channels active by default
-byte ActiveChannelsByte = 255; //all channels
-int ActiveChannelsList[] = {0, 1, 2, 3, 4, 5, 6, 7};
-int nActiveChannels = 8;
+const byte nPhysicalChannels = 8;
+byte nActiveChannels = 8;
 
 // Actions
 boolean StreamSignalToUSB = false; // Stream one channel to AnalogStreamer
@@ -68,7 +56,8 @@ boolean SendingDataToSerial = false; // Send adc reads through serial port
 // State variables
 byte ChannelToStream = 0;
 byte SelectedADCRange = 0;
-boolean acquiring = false; // true if acquiring from the ADC 
+boolean acquiring = false; // true if acquiring from the ADC
+uint32_t samplingRate = 1000; // in Hz 
 
 // Threshold crossing events
 boolean triggered[] = {0, 0, 0, 0, 0, 0, 0, 0};
@@ -85,27 +74,19 @@ uint8_t sdReadBuffer[sdReadBufferSize*2];
 uint16_t adcDigitalValue = 0;
 uint32_t nSamplesAcquired = 0;
 uint32_t maxSamplesToAcquire = 0; // 0 = infinite
-
-// Data from a single sample is set up at a union struct for efficient integer type conversion
-union {
-    byte uint8[16];
-    uint16_t uint16[8];
-} data;
+byte VoltageRangeArray[nPhysicalChannels] = {0};
 
 // Error messages stored in flash.
 #define error(msg) sd.errorHalt(F(msg))
 
 void setup() {
-
-  pinMode(adcChipSelectPin, OUTPUT); //DISCARD
-  digitalWrite(adcChipSelectPin, HIGH); //DISCARD
   pinMode(DebugPin, OUTPUT);
   digitalWrite(DebugPin, HIGH);
-  Serial2.begin(1312500); // Initialize UART serial port to talk to bpod
-  Serial3.begin(1312500); // Initialize UART serial port to talk to bpod
+  Serial2.begin(1312500); 
+  Serial3.begin(1312500);
   SPI.begin();
   SD.begin(); // Initialize microSD card
-  hardwareTimer.begin(handler, 1000); // hardwareTimer is an interval timer object - Teensy 3.6's hardware timer
+  hardwareTimer.begin(handler, (1/(double)samplingRate)*1000000); // hardwareTimer is an interval timer object - Teensy 3.6's hardware timer
 }
 
 
@@ -114,12 +95,12 @@ void loop() {
 }
 
 void handler(void) {
-  if (Serial1COM.available() > 0) {
-    opCode = Serial1COM.readByte(); // Read in an op code
+  if (StateMachineCOM.available() > 0) {
+    opCode = StateMachineCOM.readByte(); // Read in an op code
     opSource = 1; // UART 1
     newOpCode = true;
-  } else if (Serial2COM.available() > 0) {
-    opCode = Serial2COM.readByte();
+  } else if (OutputStreamCOM.available() > 0) {
+    opCode = OutputStreamCOM.readByte();
     opSource = 2; // UART 2
     newOpCode = true;
   } else if (USBCOM.available() > 0) {
@@ -133,12 +114,10 @@ void handler(void) {
   if (newOpCode) { // If an op byte arrived from one of the serial interfaces
     newOpCode = false;
     switch (opCode) {
-      
       case 'O': {
           if (opSource == 0) {
             USBCOM.writeByte(161); // Send acknowledgement byte
             USBCOM.writeUint32(FirmwareVersion); // Send firmware version
-            SetDefaultADCSettings();
             StreamSignalToUSB = false;
             StreamEventsToUSB = false;
             LoggingDataToSD = false;
@@ -169,33 +148,20 @@ void handler(void) {
         } break;
 
       case 'R': { // Select ADC Voltage range
-          byte VoltageRangeArray[8];
-          USBCOM.readByteArray(VoltageRangeArray, 8);
-          AD.setRange(VoltageRangeArray);
-
+          USBCOM.readByteArray(VoltageRangeArray, nActiveChannels);
+          for (int i = 0; i > nPhysicalChannels; i++) {
+            AD.setRange(i, VoltageRangeArray[i]);
+          }
         } break;
 
-      case 'A': { // Select active channels
-          ActiveChannelsByte = USBCOM.readByte();
-          nActiveChannels = 0;
-          int k = 0;
-          for (int i = 0; i < 8; i++) {
-            if (bitRead(ActiveChannelsByte, 7 - i) == 1) {
-              ActiveChannelsList[k] = i; // holds a list of channel numbers active (i.e. [1,3,5])
-              nActiveChannels++;
-              k++;
-            }
-          }
-
+      case 'A': { // Set number of active channels
+          nActiveChannels = USBCOM.readByte();
           USBCOM.writeByte(1); // Send confirm byte
         } break;
 
       case 'E': { // Start streaming events
           StreamEventsToUSB = true;
-          byte SequenceByte = 0b11111111;
-          AD.setSequence(SequenceByte);
-          
-          for (int i = 0; i < 8; i++) {
+          for (int i = 0; i < nPhysicalChannels; i++) {
             triggered[i] = 0;
           }
         } break;
@@ -206,25 +172,23 @@ void handler(void) {
         } break;
 
       case 'B': { // Set reset values
-          for (int i = 0; i < 8; i++) {
+          for (int i = 0; i < nPhysicalChannels; i++) {
             ResetValue[i] = USBCOM.readUint32();
           }
-
         } break;
 
       case 'T': { // Set thresholds
-          for (int i = 0; i < 8; i++) {
+          for (int i = 0; i < nPhysicalChannels; i++) {
             ThresholdValue[i] = USBCOM.readUint32();
           }
         } break;
 
       case 'L': { // Start logging data
-          Serial1COM.writeChar(1); // Send start logging flag to bpod
+          //StateMachineCOM.writeChar(1); // Uncomment to send start logging flag to bpod for diagnostics (NOTE: may be confused with a threshold event if streaming events)
           StartLogData();
         } break;
 
       case 'Z': { // Stop  logging data
-          Serial1COM.writeChar(2); // Send start logging flag to bpod
           StopLogData();
           stopAcquiringIfNotActive();
         } break;
@@ -255,16 +219,15 @@ void handler(void) {
         } break;
 
       case 'P': { // Change sampling frequency
-          unsigned long samplingPeriod = USBCOM.readUint32();
+          samplingRate = USBCOM.readUint32();
           hardwareTimer.end();
-          hardwareTimer.begin(handler, samplingPeriod);
+          hardwareTimer.begin(handler, (1/(double)samplingRate)*1000000);
         } break;
 
       case 'N': { // Start sending threshold crossing events to bpod
-          for (int i = 0; i < 8; i++) {
+          for (int i = 0; i < nPhysicalChannels; i++) {
             triggered[i] = 0;
           }
-          SetupSequenceRead(ActiveChannelsByte);
           SendingEventsToBpod = true;
           acquiring = true;
         } break;
@@ -275,7 +238,6 @@ void handler(void) {
         } break;
 
       case 'H': { // Start sending adc reads to serial port
-          SetupSequenceRead(ActiveChannelsByte);
           SendingDataToSerial = true;
           acquiring = true;
         } break;
@@ -285,9 +247,6 @@ void handler(void) {
           stopAcquiringIfNotActive();
         } break;
 
-      case 'Q': { // Disconnect from client
-
-        } break;
       case 'W': {
         maxSamplesToAcquire = USBCOM.readUint32();
         USBCOM.writeByte(1);
@@ -295,19 +254,20 @@ void handler(void) {
     }// end switch(opCode)
   }// end newOpCode
 
+  if (acquiring) {
+    AD.readADC(); // Reads all active channels and stores the result in a buffer in the AD object: AD.analogData[]
+  }
 
   // Streaming data with AnalogStreamer
   if (StreamSignalToUSB) {
-    USBCOM.writeUint16(readOneChannel(ChannelToStream));
+    USBCOM.writeUint16(AD.analogData.uint16[ChannelToStream]);
   }
 
   // Streaming threshold crossings with AnalogStreamer
   if (StreamEventsToUSB) {
     USBCOM.writeUint8(StreamThresholdCrossing());
   }
-  if (acquiring) {
-    AD.readActiveChannels(data.uint16, nActiveChannels);
-  }
+  
   if (LoggingDataToSD) {
     LogData();
   }
@@ -319,38 +279,19 @@ void handler(void) {
   }
 } // End main timer loop
 
-
-void SetDefaultADCSettings() {
-
-  word ControlData = 12416;
-  word SequenceData = 65504;
-  word Range1Data = 49120;
-  word Range2Data = 57312;
-
-  AD.programADC(ControlData, SequenceData, Range1Data, Range2Data);
-}
-
-//Function to read one channel of the ADC, accepts the channel to be read.
-int16_t readOneChannel(int channel) {
-  return AD.readOneChannel(channel);
-}
-
 // Detect thresholds in active channels and send events to Bpod
-//void SendThresholdCrossingEvents(unsigned long data[]){
 void SendThresholdCrossingEvents() {
 
   // nActiveChannels has the number of active channels
-  // ActiveChannelsList has a list of channel numbers (0-7) that are active (sorted in ascenting order)
-
   for (int i = 0; i < nActiveChannels; i++) {
-    if (triggered[ActiveChannelsList[i]] == 0) {
-      if (data.uint16[i] > ThresholdValue[ActiveChannelsList[i]]) {
-        Serial1COM.writeByte(ActiveChannelsList[i] + 1);
-        triggered[ActiveChannelsList[i]] = 1;
+    if (triggered[i] == 0) {
+      if (AD.analogData.uint16[i] > ThresholdValue[i]) {
+        StateMachineCOM.writeByte(i + 1);
+        triggered[i] = 1;
       }
     } else {
-      if (data.uint16[i] < ResetValue[ActiveChannelsList[i]]) {
-        triggered[ActiveChannelsList[i]] = 0;
+      if (AD.analogData.uint16[i] < ResetValue[i]) {
+        triggered[i] = 0;
       }
     }
   }
@@ -358,8 +299,8 @@ void SendThresholdCrossingEvents() {
 
 // Send data to serial port
 void SendDataToSerial() {
-  Serial1COM.writeByte('R');
-  Serial1COM.writeUint16Array(data.uint16, nActiveChannels);
+  StateMachineCOM.writeByte('R');
+  StateMachineCOM.writeUint16Array(AD.analogData.uint16, nActiveChannels);
 }
 
 // Log data
@@ -367,8 +308,8 @@ void LogData() {
   //Write data to SD
   unsigned long Pos = 0;
   for (int i = 0; i < nActiveChannels; i++) {
-    DataFile.write(data.uint8[Pos]);
-    DataFile.write(data.uint8[Pos+1]);
+    DataFile.write(AD.analogData.uint8[Pos]);
+    DataFile.write(AD.analogData.uint8[Pos+1]);
     Pos += 2;
   }
   nSamplesAcquired++;
@@ -384,43 +325,13 @@ void StartLogData() {
   acquiring = true;
   SD.remove("Data.wfm");
   DataFile = SD.open("Data.wfm", FILE_WRITE);
-  SetupSequenceRead(ActiveChannelsByte);
   nSamplesAcquired = 0;
-}
-
-// Setting up chip for sequence read
-void SetupSequenceRead(byte ActiveChannelsByte) {
-
-  byte adcControlRegister_byte1 = 0b10000000;
-  byte adcControlRegister_byte2 = 0b00110100; // Sets control register for sequence
-
-  word adcSequenceRegister = (0b111 << 13) | ActiveChannelsByte << 5;
-  byte adcSequenceRegister_byte1 = highByte(adcSequenceRegister);
-  byte adcSequenceRegister_byte2 = lowByte(adcSequenceRegister);
-
-  noInterrupts(); // disable interupts to prepare to send address data to the ADC.
-
-  SPI.beginTransaction(ADCSettings);
-  digitalWrite(adcChipSelectPin, LOW); // take the Chip Select pin low to select the ADC.
-  SPI.transfer(adcSequenceRegister_byte1); //  write in the sequence register
-  SPI.transfer(adcSequenceRegister_byte2); //  write in the sequence register
-  digitalWrite(adcChipSelectPin, HIGH); // take the Chip Select pin high to de-select the ADC.
-  SPI.endTransaction();
-
-  SPI.beginTransaction(ADCSettings);
-  digitalWrite(adcChipSelectPin, LOW); // take the Chip Select pin low to select the ADC.
-  SPI.transfer(adcControlRegister_byte1); //  write in the control register
-  SPI.transfer(adcControlRegister_byte2); //  write in the control register
-  digitalWrite(adcChipSelectPin, HIGH); // take the Chip Select pin high to de-select the ADC.
-  SPI.endTransaction();
-
-  interrupts(); // Enable interupts.
 }
 
 void StopLogData() {
 
   LoggingDataToSD = false;
-  Serial1COM.writeByte(10); // Send stop logging flag to bpod
+  StateMachineCOM.writeByte(10); // Send stop logging flag to bpod
 
   // Close logging file
   DataFile.close();
@@ -434,24 +345,10 @@ void stopAcquiringIfNotActive() {
 
 //Function to detect thresholds read all channels of the ADC in sequence
 byte StreamThresholdCrossing() {
-
   byte ThresholdCrossed = 0;
-
-  noInterrupts(); // disable interupts to prepare to send address data to the ADC.
-
-  for (int i = 0; i < 8; i++) {
-
-    SPI.beginTransaction(ADCSettings);
-    digitalWrite(adcChipSelectPin, LOW); // take the Chip Select pin low to select the ADC.
-    byte adcDataIn_byte1 = SPI.transfer(0b00000000); // read conversion, also sending 0 as this doesn't matter.
-    byte adcDataIn_byte2 = SPI.transfer(0b00000000); // read conversion, also sending 0 as this doesn't matter.
-    digitalWrite(adcChipSelectPin, HIGH); // take the Chip Select pin high to de-select the ADC.
-    SPI.endTransaction();
-
-    adcDigitalValue = (int(adcDataIn_byte1 & adcValueMask_byte1)) << 8 | (adcDataIn_byte2 & adcValueMask_byte2);
-
+  for (int i = 0; i < nPhysicalChannels; i++) {
     if (triggered[i] == 0) {
-      if (adcDigitalValue > ThresholdValue[i]) {
+      if (AD.analogData.uint16[i] > ThresholdValue[i]) {
         ThresholdCrossed = ThresholdCrossed | 1 << i;
         triggered[i] = 1;
       } else {
@@ -460,22 +357,19 @@ byte StreamThresholdCrossing() {
     } else {
       ThresholdCrossed = ThresholdCrossed | 0 << i;
 
-      if (adcDigitalValue < ResetValue[i]) {
+      if (AD.analogData.uint16[i] < ResetValue[i]) {
         triggered[i] = 0;
       }
     }
   }
-
-  interrupts(); // Enable interupts.
-
   return ThresholdCrossed; // Returns the value from the function
 }
 
 void returnModuleInfo() {
-  Serial1COM.writeByte(65); // Acknowledge
-  Serial1COM.writeUint32(FirmwareVersion); // 4-byte firmware version
-  Serial1COM.writeByte(sizeof(moduleName) - 1); // Length of module name
-  Serial1COM.writeCharArray(moduleName, sizeof(moduleName) - 1); // Module name
-  Serial1COM.writeByte(0);
+  StateMachineCOM.writeByte(65); // Acknowledge
+  StateMachineCOM.writeUint32(FirmwareVersion); // 4-byte firmware version
+  StateMachineCOM.writeByte(sizeof(moduleName) - 1); // Length of module name
+  StateMachineCOM.writeCharArray(moduleName, sizeof(moduleName) - 1); // Module name
+  StateMachineCOM.writeByte(0);
 }
 
